f0892b578f2059cc2c6f12a2c1739f08a47cc3bb
User <1900roleplay@gmail.com>
2026-04-11 14:36:24 +0200

    initial commit

---
+++ .history/kernel_20260411143332.asm
@@ -0,0 +1,20 @@
+bits 32
+section .text
+        ;multiboot spec
+        align 4
+        dd 0x1BADB002
+        dd 0x00
+        dd - (0x1BADB002 + 0x00)
+
+global start
+extern kmain
+
+start:
+  cli
+  mov esp, stack_space
+  call kmain
+  hlt
+
+section .bss
+resb 8192
+stack_space:
