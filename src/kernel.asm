819ee86d95036cd0b7dc6e3526337236530a7719
jll4n <julianpoulainpro@gmail.com>
2026-04-12 11:59:06 +0200

    ajout clavier à fix

--- src/kernel.asm
+++ src/kernel.asm
@@ -8,6 +8,13 @@ section .text
 global start
 extern kmain
 
+global read_port
+global write_port
+global load_idt
+global keyboard_handler
+
+extern keyboard_handler_main
+
 start:
     cli
     mov esp, stack_space
@@ -24,6 +31,16 @@ write_port:
 	mov   al, [esp + 4 + 4]  
 	out   dx, al  
 	ret
+
+keyboard_handler:
+    call keyboard_handler_main
+    iretd
+
+load_idt:
+    mov edx, [esp + 4]
+    lidt [edx]
+    sti
+    ret
 
 section .bss
 resb 8192
--- src/kernel.c
+++ src/kernel.c
@@ -1,3 +1,103 @@
+#include "keyboard_map.h"
+
+extern void load_idt(unsigned long *idt_ptr);
+extern unsigned char read_port(unsigned short port);
+extern void write_port(unsigned short port, unsigned char data);
+extern void keyboard_handler();
+
+struct IDT_entry {
+    unsigned short int offset_lowerbits;
+    unsigned short int selector;
+    unsigned char zero;
+    unsigned char type_attr;
+    unsigned short int offset_higherbits;
+};
+
+struct IDT_entry IDT[256];
+
+void idt_init(void)
+{
+	unsigned long keyboard_address;
+	unsigned long idt_address;
+	unsigned long idt_ptr[2];
+
+	/* populate IDT entry of keyboard's interrupt */
+	keyboard_address = (unsigned long)keyboard_handler; 
+	IDT[0x21].offset_lowerbits = keyboard_address & 0xffff;
+	IDT[0x21].selector = 0x08; /* KERNEL_CODE_SEGMENT_OFFSET */
+	IDT[0x21].zero = 0;
+	IDT[0x21].type_attr = 0x8e; /* INTERRUPT_GATE */
+	IDT[0x21].offset_higherbits = (keyboard_address & 0xffff0000) >> 16;
+	
+
+	/*     Ports
+	*	 PIC1	PIC2
+	*Command 0x20	0xA0
+	*Data	 0x21	0xA1
+	*/
+
+	/* ICW1 - begin initialization */
+	write_port(0x20 , 0x11);
+	write_port(0xA0 , 0x11);
+
+	/* ICW2 - remap offset address of IDT */
+	/*
+	* In x86 protected mode, we have to remap the PICs beyond 0x20 because
+	* Intel have designated the first 32 interrupts as "reserved" for cpu exceptions
+	*/
+	write_port(0x21 , 0x20);
+	write_port(0xA1 , 0x28);
+
+	/* ICW3 - setup cascading */
+	write_port(0x21 , 0x00);  
+	write_port(0xA1 , 0x00);  
+
+	/* ICW4 - environment info */
+	write_port(0x21 , 0x01);
+	write_port(0xA1 , 0x01);
+	/* Initialization finished */
+
+	/* mask interrupts */
+	write_port(0x21 , 0xff);
+	write_port(0xA1 , 0xff);
+
+	/* fill the IDT descriptor */
+	idt_address = (unsigned long)IDT ;
+	idt_ptr[0] = (sizeof (struct IDT_entry) * 256) + ((idt_address & 0xffff) << 16);
+	idt_ptr[1] = idt_address >> 16 ;
+
+	load_idt(idt_ptr);
+}
+
+void kb_init(void)
+{
+    write_port(0x21, 0xFD); // active IRQ1
+}
+
+void keyboard_handler_main(void)
+{
+    unsigned char status;
+    char keycode;
+
+    write_port(0x20, 0x20); // EOI
+
+    status = read_port(0x64);
+
+    if (status & 0x01) {
+        keycode = read_port(0x60);
+
+        if (keycode < 0)
+            return;
+
+        // affichage simple (à adapter)
+        char *vidptr = (char*)0xb8000;
+        static int pos = 0;
+
+        vidptr[pos++] = keyboard_map[keycode];
+        vidptr[pos++] = 0x07;
+    }
+}
+
 void kmain(void)
 {
 	const char *str = "jOS";
@@ -5,6 +105,9 @@ void kmain(void)
 	unsigned int i = 0;
 	unsigned int j = 0;
 
+	idt_init();
+	kb_init();
+	
 	while(j < 80 * 25 * 2) {
 		vidptr[j] = ' ';
 		vidptr[j+1] = 0x07; 		
